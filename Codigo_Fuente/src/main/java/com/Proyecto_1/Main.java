257401238e895761afa16fed391fda761777a16e
KevinLo8 <kjle10.97@gmail.com>
2024-09-24 14:30:58 -0600

    actualizacion

--- Codigo_Fuente/src/main/java/com/Proyecto_1/Main.java
+++ Codigo_Fuente/src/main/java/com/Proyecto_1/Main.java
@@ -1,8 +1,11 @@
 package com.Proyecto_1;
 
+import com.Proyecto_1.Frontend.FramePrincipal;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        FramePrincipal framePrincipal = new FramePrincipal();
+        framePrincipal.setVisible(true);
     }
 }
